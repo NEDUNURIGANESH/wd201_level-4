8854e652519f34af4b96dcfa2cd5566cafabc4d0
NEDUNURIGANESH <bobbybablu001@gmail.com>
2022-11-18 22:44:36 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,34 +1,5 @@
 /* eslint-disable no-undef */
-/*const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
-
-describe("Todolist Test Suite", () => {
-  beforeAll(() => {
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-  });
-  test("Should add new todo", () => {
-    const todoItemsCount = all.length;
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-    expect(all.length).toBe(todoItemsCount + 1);
-  });
-
-  test("Should mark a todo as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
-  });
-});
-*/
-/* eslint-disable no-undef */
 const todoList = require("../todo");
 let today = new Date().toLocaleDateString("en-CA");
 
@@ -41,7 +12,6 @@ describe("Todolist Test Suite", () => {
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
-    
   });
 
   test("Should add new todo", () => {
@@ -71,7 +41,7 @@ describe("Todolist Test Suite", () => {
     ).toBe(true);
   });
 
-  test("dueToday todos ", () => {
+  test("dueToday todos", () => {
     let listOfTodos = dueToday();
 
     expect(
